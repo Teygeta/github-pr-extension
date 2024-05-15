cdcf44743f33ea9d57714d0bd64e31dd7b691e29
Vittorio <98349901+Teygeta@users.noreply.github.com>
2024-05-15 12:06:20 +0200

    bundle

--- dist/content.bundle.ts
+++ dist/content.bundle.ts
@@ -3,7 +3,7 @@ var __webpack_exports__ = {};
 const isGitHubPullRequestPage = () => /.*github.com\/.*\/pull\/.*/.test(window.location.href)
 
 const titleInput = document.querySelector('.form-control.js-quick-submit')
-const editSaveButtonsContainer = document.querySelector('.js-issue-update.js-comment')
+const editPRButtonsContainer = document.querySelector('.js-issue-update.js-comment')
 
 const aiMagicButton = document.createElement('button')
 aiMagicButton.innerText = 'AI'
@@ -17,7 +17,7 @@ async function fetchFiles() {
   const currentPrNumber = window.location.href.split('pull/')[1].split('/')[0]
   const repoPath = window.location.href.split('github.com/')[1].split('/pull')[0]
 
-  const accessToken = "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"
+  const accessToken = "MISSING_ENV_VAR".GITHUB_ACCESS_TOKEN
 
   try {
     const response = await fetch(`https://api.github.com/repos/${repoPath}/pulls/${currentPrNumber}/files`, {
@@ -25,6 +25,11 @@ async function fetchFiles() {
         Authorization: `token ${accessToken}`,
       },
     })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    
     const data = await response.json()
 
     return data
@@ -47,8 +52,8 @@ document.addEventListener('click', async (event) => {
     return
   }
 
-  if (editSaveButtonsContainer) {
-    editSaveButtonsContainer.appendChild(aiMagicButton)
+  if (editPRButtonsContainer) {
+    editPRButtonsContainer.appendChild(aiMagicButton)
     if (event.target === aiMagicButton) {
       if (titleInput instanceof HTMLInputElement) {
         const output = await getTitleOutput()
